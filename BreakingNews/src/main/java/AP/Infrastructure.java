fb44e7976c3c0a9f592ca0ba9f6386ce63869fd5
Amir Mehdi <amirmehdiimani@icloud.com>
2025-03-13 15:36:52 +0330

    file new

--- BreakingNews/src/main/java/AP/Infrastructure.java
+++ BreakingNews/src/main/java/AP/Infrastructure.java
@@ -6,19 +6,25 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
+import java.time.LocalDate;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Infrastructure {
 
     private final String URL;
-    private final String APIKEY;
+    private String APIKEY;
     private final String JSONRESULT;
-    private ArrayList<News> newsList; // TODO: Create the News class
-
+    private ArrayList<News> newsList;
 
     public Infrastructure(String APIKEY) {
         this.APIKEY = APIKEY;
-        this.URL = "https://newsapi.org/v2/everything?q=tesla&from=2025-02-05&sortBy=publishedAt&apiKey=";
+        this.URL = "https://newsapi.org/v2/everything?q=tesla&from=" + LocalDate.now().minusDays(1) + "&sortBy=publishedAt&apiKey=";
         this.JSONRESULT = getInformation();
+        this.newsList = new ArrayList<>();
+        if (this.JSONRESULT != null) {
+            parseInformation();
+        }
     }
 
     public ArrayList<News> getNewsList() {
@@ -28,7 +34,6 @@ public class Infrastructure {
     private String getInformation() {
         try {
             HttpClient client = HttpClient.newHttpClient();
-
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(URL + APIKEY))
                     .build();
@@ -46,15 +51,49 @@ public class Infrastructure {
     }
 
     private void parseInformation() {
-        // TODO: Get the first 20 news from the articles array of the json result
-        //  and parse the information of each on of them to be mapped to News class
-        //  finally add them to newsList in this class to display them in the output
+        try {
+            JSONObject jsonObject = new JSONObject(JSONRESULT);
+            JSONArray articles = jsonObject.getJSONArray("articles");
+
+            for (int i = 0; i < 20 && i < articles.length(); i++) {
+                JSONObject article = articles.getJSONObject(i);
+
+                String title = article.getString("title");
+                String description = article.getString("description");
+                String sourceName = article.getJSONObject("source").getString("name");
+                String author = article.optString("author", "Unknown");
+                String url = article.getString("url");
+                String publishedAt = article.getString("publishedAt");
+
+                News news = new News(title, description, sourceName, author, url, publishedAt);
+                newsList.add(news);
+            }
+        } catch (Exception e) {
+            System.out.println("Error parsing the JSON result: " + e.getMessage());
+        }
     }
 
     public void displayNewsList() {
-        // TODO: Display titles of the news you got from api
-        //  and print them in a way that user can choose one
-        //  to see the full information of the news
+        if (newsList.isEmpty()) {
+            System.out.println("No news available.");
+            return;
+        }
+
+        System.out.println("Select a news article to view details:");
+        for (int i = 0; i < newsList.size(); i++) {
+            System.out.println((i + 1) + ". " + newsList.get(i).getTitle());
+        }
     }
 
+    public void displayFullNews(int index) {
+        if (index < 0 || index >= newsList.size()) {
+            System.out.println("Invalid selection.");
+            return;
+        }
+
+        News selectedNews = newsList.get(index);
+        selectedNews.displayNews();
+    }
 }
+
+
